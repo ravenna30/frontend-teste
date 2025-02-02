c7f4a1893c03f4ff979bee55a8a4c7809e30317e
ravenna <taylaravennaaraujo77@gmail.com>
2025-02-01 21:18:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,4 @@
-body {
-    font-family: Arial, sans-serif;
-    background-color: #f0f0f0;
-    color: #333;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    text-align: center;
-}
+document.getElementById("changeTextButton").addEventListener("click", function() {
+    document.querySelector("h1").textContent = "Você clicou no botão!";
+});
 
-h1 {
-    color: #3498db;
-}
-
-button {
-    background-color: #3498db;
-    color: white;
-    font-size: 1rem;
-    padding: 10px 20px;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    transition: background-color 0.3s;
-}
-
-button:hover {
-    background-color: #2980b9;
-}
